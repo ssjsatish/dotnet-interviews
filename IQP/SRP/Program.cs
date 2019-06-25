6478328bee6285e82d5dc9ab1678c867cbf70145
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-06-25 12:54:38 +0530

    local commit

--- IQP/SRP/Program.cs
+++ IQP/SRP/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace SRP
 {
+    /// <summary>
+    /// This class is to show how single responsibility principle works in real world.
+    /// 1. A class or method should have only one reason to change.
+    /// 2. A class or method should have only a single responsilbility.
+    /// 3. When a class has more than one responsibility, there are also more reasons to change that class
+    /// 4. Benefits as follows:
+    /// 5. Reduces the complexity of a code
+    /// 6. Increases readability
+    /// 7. Reusability and reduces error
+    /// 8. Increases testability
+    /// 9. Reduces coupling
+    /// 10. 
+    /// </summary>
     class Program
     {
+
         static void Main(string[] args)
         {
+
         }
     }
 }
