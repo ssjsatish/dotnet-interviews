4053de5c59f9ffced45d80012720623d2042b31d
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-06-27 18:19:16 +0530

    signal R example

---
+++ IQP/SignalR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
