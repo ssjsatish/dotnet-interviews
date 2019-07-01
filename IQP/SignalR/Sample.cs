4f664d6c910be5c11959f5d0f2f444504f0ef83e
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-07-01 12:38:09 +0530

    added signalR project

--- IQP/SignalR/Sample.cs
+++ IQP/SignalR/Sample.cs
@@ -6,16 +6,11 @@ using System.Threading.Tasks;
 
 namespace SignalR
 {
-    class Program
+    class Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First Class {0}", args[0]);
-        }
-        static int Main(int a=5)
-        {
-            Console.WriteLine("Ha HA {0}", a);
-            return 0;
+            Console.WriteLine("Sample Class");
         }
     }
 }
